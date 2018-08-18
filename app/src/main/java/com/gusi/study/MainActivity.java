cc0fba8bc9b44690a8ffc4af82a70a3a5dcef74d
18609573806@126.com <18609573806@126.com>
2018-08-18 17:14:20 +0800

    svg 21 使用 Animation.ABSOLUTE  200 Animation.RELATIVE_TO_SELF  2 Animation.RELATIVE_TO_PARENT  0.2f

--- app/src/main/java/com/gusi/study/MainActivity.java
+++ app/src/main/java/com/gusi/study/MainActivity.java
@@ -1,6 +1,9 @@
 package com.gusi.study;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.KeyEvent;
@@ -8,43 +11,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import com.gusi.study.Lottie.LottieActivity;
-import com.gusi.study.ScrollTv.ScrollTvActivity;
-import com.gusi.study.anim.AnimActivity;
 import com.gusi.study.base.BaseActivity;
-import com.gusi.study.bigrcv.BigRcvActivity;
-import com.gusi.study.canvas.CanvasActivity;
-import com.gusi.study.constraint.Constraint1Activity;
-import com.gusi.study.constraint.ConstraintActivity;
-import com.gusi.study.dragshadow.DragShadowActivity;
-import com.gusi.study.drawable.DrawableActivity;
-import com.gusi.study.excel.ExcelActivity;
-import com.gusi.study.flippertv.FlipperTvActivity;
-import com.gusi.study.floating.FloatingActivity;
-import com.gusi.study.flow.FlowActivity;
-import com.gusi.study.formlayout.FormActivity;
-import com.gusi.study.formlayout.horizontalweight.HorizontalWeightActivity;
-import com.gusi.study.granzort.GranzortActivity;
-import com.gusi.study.intentservice.IntentServiceActivity;
-import com.gusi.study.keyboard.KeyBoardActivity;
-import com.gusi.study.loading.LoadingActivity;
-import com.gusi.study.nestedscroll.NestedScroll1Activity;
-import com.gusi.study.nestedscroll.ScrollingActivity;
-import com.gusi.study.piechart.PieChartActivity;
-import com.gusi.study.protobuf.ProtobufActivity;
-import com.gusi.study.rainbow.RainbowActivity;
-import com.gusi.study.screenshot.ScreenShotActivity;
-import com.gusi.study.sheet.BottomSheetActivity;
-import com.gusi.study.span.SpanActivity;
-import com.gusi.study.surface.SurfaceActivity;
-import com.gusi.study.swipe.SwipeActivity;
-import com.gusi.study.threadlocal.ThreadLocalActivity;
-import com.gusi.study.today.TodayActivity;
-import com.gusi.study.transparent.TransparentActivity;
-import com.gusi.study.vlayout.VLayoutActivity;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import butterknife.BindView;
@@ -55,7 +26,8 @@ import butterknife.BindView;
 public class MainActivity extends BaseActivity {
     @BindView(R.id.rcv)
     RecyclerView mRcv;
-    List<String> list = new ArrayList<>();
+    private List<ActivityInfo> mActivityInfoList;
+    //    List<String> list = new ArrayList<>();
 
     @Override
     protected int getLayout() {
@@ -69,157 +41,31 @@ public class MainActivity extends BaseActivity {
         mRcv.setLayoutManager(new LinearLayoutManager(this));
         mRcv.setHasFixedSize(true);
         mRcv.setAdapter(new Adapter());
-
     }
 
     private void initData() {
-        list.add("PieChartView");
-        list.add("Drawable");
-        list.add("FlowLayout");
-        list.add("Constraint");
-        list.add("Constraint1");
-        list.add("FormLayout");
-        list.add("ScrollTv");
-        list.add("Loading");
-        list.add("Granzort");
-        list.add("KeyBoard");
-        list.add("FloatBall");
-        list.add("FlipperTv");
-        list.add("TodayTabLayout");
-        list.add("NestedScroll");
-        list.add("Scroll");
-        list.add("Rainbow");
-        list.add("VLayout");
-        list.add("Span");
+        PackageManager pm = getPackageManager();
+        try {
+            String packageName = getPackageName();
+            PackageInfo packageInfo = pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+            mActivityInfoList = new ArrayList<>();
+            for (ActivityInfo activityInfo : packageInfo.activities) {
+                if (!activityInfo.name.contains(getPackageName())) continue;
+                mActivityInfoList.add(activityInfo);
+            }
 
-        list.add("ThreadLocal");
-        list.add("Swipe");
-        list.add("Transparent");
-        list.add("BigRcv");
-        list.add("Canvas");
-        list.add("Lottie");
-        list.add("DragShadow");
-        list.add("ScreenShot");
-        list.add("HorizontalWeight");
-        list.add("Protobuf");
-        list.add("Excel");
-        list.add("BottomSheet");
-        list.add("Anim");
-        list.add("Surface");
-        list.add("IntentService");
-
-        Collections.sort(list);
-    }
-
-    private void itemClick(String txt) {
-        Intent intent = new Intent();
-        switch (txt) {
-            case "PieChartView":
-                intent.setClass(this, PieChartActivity.class);
-                break;
-            case "Drawable":
-                intent.setClass(this, DrawableActivity.class);
-                break;
-            case "FlowLayout":
-                intent.setClass(this, FlowActivity.class);
-                break;
-            case "Constraint":
-                intent.setClass(this, ConstraintActivity.class);
-                break;
-            case "Constraint1":
-                intent.setClass(this, Constraint1Activity.class);
-                break;
-            case "FormLayout":
-                intent.setClass(this, FormActivity.class);
-                break;
-            case "ScrollTv":
-                intent.setClass(this, ScrollTvActivity.class);
-                break;
-            case "Loading":
-                intent.setClass(this, LoadingActivity.class);
-                break;
-            case "Granzort":
-                intent.setClass(this, GranzortActivity.class);
-                break;
-            case "KeyBoard":
-                intent.setClass(this, KeyBoardActivity.class);
-                break;
-
-            case "FloatBall":
-                intent.setClass(this, FloatingActivity.class);
-                break;
-            case "FlipperTv":
-                intent.setClass(this, FlipperTvActivity.class);
-                break;
-            case "TodayTabLayout":
-                intent.setClass(this, TodayActivity.class);
-                break;
-            case "NestedScroll":
-                intent.setClass(this, NestedScroll1Activity.class);
-                break;
-
-            case "Scroll":
-                intent.setClass(this, ScrollingActivity.class);
-                break;
-            case "Rainbow":
-                intent.setClass(this, RainbowActivity.class);
-                break;
-            case "VLayout":
-                intent.setClass(this, VLayoutActivity.class);
-                break;
-            case "Span":
-                intent.setClass(this, SpanActivity.class);
-                break;
-
-            case "ThreadLocal":
-                intent.setClass(this, ThreadLocalActivity.class);
-                break;
-            case "Swipe":
-                intent.setClass(this, SwipeActivity.class);
-                break;
-            case "Transparent":
-                intent.setClass(this, TransparentActivity.class);
-                break;
-            case "BigRcv":
-                intent.setClass(this, BigRcvActivity.class);
-                break;
-
-            case "Canvas":
-                intent.setClass(this, CanvasActivity.class);
-                break;
-            case "Lottie":
-                intent.setClass(this, LottieActivity.class);
-                break;
-            case "DragShadow":
-                intent.setClass(this, DragShadowActivity.class);
-                break;
-            case "ScreenShot":
-                intent.setClass(this, ScreenShotActivity.class);
-                break;
-            case "HorizontalWeight":
-                intent.setClass(this, HorizontalWeightActivity.class);
-                break;
-            case "Protobuf":
-                intent.setClass(this, ProtobufActivity.class);
-                break;
-            case "Excel":
-                intent.setClass(this, ExcelActivity.class);
-                break;
-            case "BottomSheet":
-                intent.setClass(this, BottomSheetActivity.class);
-                break;
-            case "Anim":
-                intent.setClass(this, AnimActivity.class);
-                break;
-            case "Surface":
-                intent.setClass(this, SurfaceActivity.class);
-                break;
-            case "IntentService":
-                intent.setClass(this, IntentServiceActivity.class);
-                break;
+            Collections.sort(mActivityInfoList, new Comparator<ActivityInfo>() {
+                @Override
+                public int compare(ActivityInfo o1, ActivityInfo o2) {
+                    return o1.name.substring(o1.name.lastIndexOf(".") + 1, o1.name.length()).replace("Activity", "")
+                            .compareTo(o2.name.substring(o2.name.lastIndexOf(".") + 1, o2.name.length()).replace("Activity", ""));
+                }
+            });
+        } catch (PackageManager.NameNotFoundException e) {
         }
-        startActivity(intent);
+
     }
+
 
     class Adapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
@@ -231,7 +77,6 @@ public class MainActivity extends BaseActivity {
                             ViewGroup.LayoutParams.WRAP_CONTENT);
             btn.setLayoutParams(params);
             btn.setPadding(0, 10, 0, 10);
-
             return new RecyclerView.ViewHolder(btn) {
 
             };
@@ -240,18 +85,27 @@ public class MainActivity extends BaseActivity {
         @Override
         public void onBindViewHolder(final RecyclerView.ViewHolder holder, final int position) {
             final Button btn = (Button) holder.itemView;
-            btn.setText(list.get(position));
+            final ActivityInfo activityInfo = mActivityInfoList.get(position);
+            String name = activityInfo.name;
+            name = name.substring(name.lastIndexOf(".") + 1, name.length()).replace("Activity", "");
+            btn.setText(name);
             btn.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    itemClick(btn.getText().toString());
+                    ClassLoader classLoader = getClassLoader();
+                    try {
+                        Class<?> aClass = classLoader.loadClass(activityInfo.name);
+                        startActivity(new Intent(MainActivity.this, aClass));
+                    } catch (ClassNotFoundException e) {
+                    }
+
                 }
             });
         }
 
         @Override
         public int getItemCount() {
-            return list.size();
+            return mActivityInfoList.size();
         }
     }
 
